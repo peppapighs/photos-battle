d878df8ace34841f53c8a7a151c2bba918e52d2e
peppapighs <ptk.prasertsuk@gmail.com>
2022-11-11 00:24:21 +0800

    :sparkles: add API endpoint for fetching album contents

---
+++ pages/api/album/[id]/index.ts
@@ -0,0 +1,130 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+import { unstable_getServerSession } from 'next-auth'
+import { z } from 'zod'
+
+import prisma from 'lib/prismadb'
+import { AlbumContents, AlbumList, MediaList } from 'types/google'
+import getGoogleAccessToken from 'utils/getGoogleAccessToken'
+
+import { nextAuthOptions } from '../../auth/[...nextauth]'
+
+const PHOTOS_PER_PAGE = 25
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method, query, body } = req
+  const session = await unstable_getServerSession(req, res, nextAuthOptions)
+
+  const parsedQuery = z
+    .object({ id: z.string(), pageToken: z.string().optional() })
+    .safeParse(query)
+  if (!parsedQuery.success) {
+    res.status(400).end('Bad Request')
+    return
+  }
+  const { id: albumId, pageToken } = parsedQuery.data
+
+  if (!session?.user) {
+    res.status(401).end('Unauthorized')
+    return
+  }
+
+  const album = await prisma.album.findUnique({ where: { id: albumId } })
+  if (!album) {
+    res.status(404).end('Not Found')
+    return
+  }
+  if (album.userId !== session.user.id) {
+    res.status(401).end('Unauthorized')
+    return
+  }
+
+  const accessToken = await getGoogleAccessToken(session.user.id)
+  if (!accessToken) {
+    res.status(401).end('Access token not found')
+    return
+  }
+
+  if (method === 'GET') {
+    const photos = await prisma.photo.findMany({
+      take: PHOTOS_PER_PAGE,
+      ...(pageToken &&
+        pageToken !== '' && { skip: 1, cursor: { id: pageToken } }),
+      where: { albumId },
+      orderBy: [{ rating: 'desc' }, { id: 'asc' }],
+    })
+
+    if (photos.length === 0) {
+      res.status(200).json({ mediaItems: [] })
+      return
+    }
+
+    const url =
+      'https://photoslibrary.googleapis.com/v1/mediaItems:batchGet?' +
+      new URLSearchParams(photos.map(photo => ['mediaItemIds', photo.photoId]))
+
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+
+    if (!response.ok) {
+      res.status(500).end('Unable to fetch photos in album')
+      return
+    }
+
+    const data = await response.json()
+    console.log(data)
+    const nextPageToken = photos[photos.length - 1].id
+    res.status(200).json(MediaList.parse({ ...data, nextPageToken }))
+  } else if (method === 'POST') {
+    const parsedBody = z
+      .object({ pageToken: z.string().optional() })
+      .safeParse(body)
+    if (!parsedBody.success) {
+      res.status(400).end('Bad Request')
+      return
+    }
+    const { pageToken } = parsedBody.data
+
+    const url = 'https://photoslibrary.googleapis.com/v1/mediaItems:search'
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({
+        pageSize: PHOTOS_PER_PAGE,
+        albumId,
+        ...(pageToken && { pageToken }),
+      }),
+    })
+
+    if (!response.ok) {
+      res.status(500).end('Unable to fetch album contents')
+      return
+    }
+
+    const data = AlbumContents.parse(await response.json())
+    await prisma.photo.createMany({
+      data: data.mediaItems.map(photo => ({
+        photoId: photo.id,
+        albumId,
+        userId: session.user.id,
+      })),
+      skipDuplicates: true,
+    })
+
+    res.status(200).json(data)
+  } else {
+    res.setHeader('Allow', ['GET', 'POST'])
+    res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
--- types/google.ts
+++ types/google.ts
@@ -16,3 +16,33 @@ export const AlbumList = z.object({
 })
 
 export type AlbumList = z.infer<typeof AlbumList>
+
+export const Media = z.object({
+  id: z.string(),
+  baseUrl: z.string(),
+  productUrl: z.string(),
+  mimeType: z.string(),
+  filename: z.string(),
+  mediaMetadata: z.record(z.any()),
+})
+
+export type Media = z.infer<typeof Media>
+
+export const MediaList = z
+  .object({
+    mediaItemResults: z.array(z.object({ mediaItem: Media })),
+    nextPageToken: z.string().optional(),
+  })
+  .transform(data => ({
+    mediaItems: data.mediaItemResults.map(item => item.mediaItem),
+    nextPageToken: data.nextPageToken,
+  }))
+
+export type MediaList = z.infer<typeof MediaList>
+
+export const AlbumContents = z.object({
+  mediaItems: z.array(Media),
+  nextPageToken: z.string().optional(),
+})
+
+export type AlbumContents = z.infer<typeof AlbumContents>
