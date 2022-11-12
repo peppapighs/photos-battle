d7ab145a63f7b302e8a6de107864440af0295cbf
peppapighs <ptk.prasertsuk@gmail.com>
2022-11-12 14:39:55 +0800

    :bug: fix menu hover color

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -77,7 +77,7 @@ export default function Navbar() {
                                   href="#"
                                   className={classNames(
                                     active
-                                      ? 'bg-gray-100 dark:bg-gray-800'
+                                      ? 'bg-gray-100 dark:bg-gray-700'
                                       : '',
                                     'block px-4 py-2 text-sm text-gray-700 transition dark:text-gray-200'
                                   )}
